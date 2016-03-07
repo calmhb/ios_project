32d643e080860e6d61b41921a47c3200833f6a22
calmhb <mac@aYolwas-3.local>
2016-03-07 10:32:20 +0800

    面向对象04

---
+++ oopDemo04/oopDemo04/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  oopDemo04
+//
+//  Created by Mac on 16/3/7.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
