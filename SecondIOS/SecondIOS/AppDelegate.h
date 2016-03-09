af5e1ca431c62469b33711b107f826482a2ea51e
calmhb <mac@ayolwas-3.local>
2016-03-09 10:17:12 +0800

    iosUI_day02

---
+++ SecondIOS/SecondIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SecondIOS
+//
+//  Created by Mac on 16/3/9.
+//  Copyright © 2016年 zk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
