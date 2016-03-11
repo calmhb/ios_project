3ece37b6a069df5fa278df68759ef78c3a46cfdc
calmhb <mac@ayolwas-3.local>
2016-03-11 11:15:43 +0800

    UIButton介绍

---
+++ ButtonUI/ButtonUI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ButtonUI
+//
+//  Created by Mac on 16/3/11.
+//  Copyright © 2016年 zk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ButtonUI/ButtonUI/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  ButtonUI
+//
+//  Created by Mac on 16/3/11.
+//  Copyright © 2016年 zk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIControl
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
